35243bf5c61221a17fd2d19ace8de94b9f7146d6
islamuddin <uddinislam46@gmail.com>
2022-07-03 16:03:24 +0500

    first class program

---
+++ 2022_07_03/first.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	printf("Hello World from PIAIC");
+	return 0;
+}
+
+// open terminal and run command 1:
+// g++ first.c -o first
+// run command 2:
+// ./first
+// output: Hello World from PIAIC
